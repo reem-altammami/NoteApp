1a72a68f50a2d4b9463d73dedb30067da3338f5b
Reem Altammami <reem.saud.ruh.k@tuwaiq.edu.sa>
2022-02-08 01:38:59 +0300

    Initial commit

---
+++ app/src/main/java/com/reem/noteapp/ui/theme/Theme.kt
@@ -0,0 +1,43 @@
+package com.reem.noteapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = LightBlue,
+    onSurface = DarkGray
+)
+
+//private val LightColorPalette = lightColors(
+//    primary = Purple500,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
+//
+//    /* Other default colors to override
+//    background = Color.White,
+//    surface = Color.White,
+//    onPrimary = Color.White,
+//    onSecondary = Color.Black,
+//    onBackground = Color.Black,
+//    onSurface = Color.Black,
+//    */
+//)
+
+@Composable
+fun NoteAppTheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+
+
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
