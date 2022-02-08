af06690ecfdc722966f8f619cdc9f159bb2edaa1
Reem Altammami <reem.saud.ruh.k@tuwaiq.edu.sa>
2022-02-09 02:40:48 +0300

    create notes ViewModel

--- app/src/main/java/com/reem/noteapp/di/AppModule.kt
+++ app/src/main/java/com/reem/noteapp/di/AppModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.reem.noteapp.feature_note.data.data_source.NoteDatabase
 import com.reem.noteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.reem.noteapp.feature_note.domain.repository.NoteRepository
+import com.reem.noteapp.feature_note.domain.use_case.AddNoteUseCase
 import com.reem.noteapp.feature_note.domain.use_case.DeleteNoteUseCase
 import com.reem.noteapp.feature_note.domain.use_case.GetNotesUseCase
 import com.reem.noteapp.feature_note.domain.use_case.NoteUseCases
@@ -39,7 +40,8 @@ object AppModule {
     fun provideUseCases(repository:NoteRepository):NoteUseCases{
         return NoteUseCases(
             getNotesUseCase = GetNotesUseCase(repository),
-            deleteNoteUseCase = DeleteNoteUseCase(repository)
+            deleteNoteUseCase = DeleteNoteUseCase(repository),
+            addNoteUseCase = AddNoteUseCase(repository)
         )
     }
 
--- app/src/main/java/com/reem/noteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/reem/noteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -2,6 +2,7 @@ package com.reem.noteapp.feature_note.domain.use_case
 
 data class NoteUseCases(
     val getNotesUseCase: GetNotesUseCase,
-    val deleteNoteUseCase: DeleteNoteUseCase
+    val deleteNoteUseCase: DeleteNoteUseCase,
+    val addNoteUseCase: AddNoteUseCase
 ) {
 }
