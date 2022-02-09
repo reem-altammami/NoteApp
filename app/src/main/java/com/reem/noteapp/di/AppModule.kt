700cdfbf2a9018fcd5786c277c8f2cac6986a322
Reem Altammami <reem.saud.ruh.k@tuwaiq.edu.sa>
2022-02-09 18:08:43 +0300

    create AddEdit viewModel

--- app/src/main/java/com/reem/noteapp/di/AppModule.kt
+++ app/src/main/java/com/reem/noteapp/di/AppModule.kt
@@ -5,10 +5,7 @@ import androidx.room.Room
 import com.reem.noteapp.feature_note.data.data_source.NoteDatabase
 import com.reem.noteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.reem.noteapp.feature_note.domain.repository.NoteRepository
-import com.reem.noteapp.feature_note.domain.use_case.AddNoteUseCase
-import com.reem.noteapp.feature_note.domain.use_case.DeleteNoteUseCase
-import com.reem.noteapp.feature_note.domain.use_case.GetNotesUseCase
-import com.reem.noteapp.feature_note.domain.use_case.NoteUseCases
+import com.reem.noteapp.feature_note.domain.use_case.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -41,7 +38,8 @@ object AppModule {
         return NoteUseCases(
             getNotesUseCase = GetNotesUseCase(repository),
             deleteNoteUseCase = DeleteNoteUseCase(repository),
-            addNoteUseCase = AddNoteUseCase(repository)
+            addNoteUseCase = AddNoteUseCase(repository),
+            getNoteUseCase = GetNoteUseCase(repository)
         )
     }
 
---
+++ app/src/main/java/com/reem/noteapp/feature_note/presentation/add_edit_note/components/AddEditNoteEvent.kt
@@ -0,0 +1,16 @@
+package com.reem.noteapp.feature_note.presentation.add_edit_note.components
+
+import androidx.compose.ui.focus.FocusState
+
+sealed class AddEditNoteEvent {
+    data class EnteredTitle(val value : String):AddEditNoteEvent()
+    data class ChangeTitleFocus(val focusState :FocusState) : AddEditNoteEvent()
+
+    data class EnteredContent(val value : String):AddEditNoteEvent()
+    data class ChangeContentFocus(val focusState :FocusState) : AddEditNoteEvent()
+
+    data class ChangColor(val color : Int): AddEditNoteEvent()
+
+    object SaveNote:AddEditNoteEvent()
+
+}
